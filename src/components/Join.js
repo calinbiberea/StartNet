155df7e0d137d50966b6848569ef319b00995c8d
Calin Biberea <calinbiberea@gmail.com>
2022-02-05 15:33:20 +0000

    feat: add landing page

---
+++ src/components/Join.js
@@ -0,0 +1,117 @@
+import React, {} from "react";
+import {
+    Button,
+    ButtonGroup, Typography,
+} from "@material-ui/core";
+import {createStyles, makeStyles} from "@material-ui/core/styles";
+import logo from "../images/logo.gif";
+
+const useStyles = makeStyles((theme) =>
+    createStyles({
+        centered: {
+            height: "100vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+        },
+        container: {
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-evenly",
+            alignItems: "center",
+        },
+        logo: {
+            marginTop: 16,
+            marginBottom: 24,
+            [theme.breakpoints.down("xs")]: {
+                height: 200,
+            },
+            [theme.breakpoints.up("sm")]: {
+                height: 300,
+            },
+        },
+        iconsAndButtonsContainer: {
+            width: "100%",
+            display: "flex",
+            justifyContent: "space-evenly",
+            alignItems: "center",
+        },
+        image: {
+            width: 350,
+            [theme.breakpoints.down("sm")]: {
+                display: "none",
+            },
+        },
+        button: {
+            margin: 8,
+            borderRadius: 20,
+            [theme.breakpoints.only("xs")]: {
+                width: 250,
+                fontSize: "1rem",
+            },
+            [theme.breakpoints.only("sm")]: {
+                width: 300,
+                fontSize: "1rem",
+            },
+            [theme.breakpoints.up("md")]: {
+                width: 320,
+                fontSize: "1.25rem",
+            },
+        },
+    })
+);
+
+
+const Join: React.FC = () => {
+    const classes = useStyles();
+
+    console.log("here we are");
+
+    return (
+        <>
+            <div className={classes.centered}>
+                <div className={classes.container}>
+                    <img className={classes.logo} src={logo} alt="StartNet"/>
+
+                    <Typography variant="h3" gutterBottom component="div">
+                        I want to...
+                    </Typography>
+
+                    <div className={classes.iconsAndButtonsContainer}>
+                        <ButtonGroup orientation="vertical">
+                            <Button
+                                className={classes.button}
+                                variant="contained"
+                                color="primary"
+                                size="large"
+                            >
+                                Invent
+                            </Button>
+
+                            <Button
+                                className={classes.button}
+                                variant="contained"
+                                color="primary"
+                                size="large"
+                            >
+                                Build
+                            </Button>
+
+                            <Button
+                                className={classes.button}
+                                variant="contained"
+                                color="primary"
+                                size="large"
+                            >
+                                Invest
+                            </Button>
+                        </ButtonGroup>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Join;
