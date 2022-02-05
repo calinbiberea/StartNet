514be3fd17b7332b075b8bd27707c3f4c3fd4a6d
Calin Biberea <calinbiberea@gmail.com>
2022-02-05 20:26:57 +0000

    feat: add edit page

---
+++ src/components/Inventors.js
@@ -0,0 +1,73 @@
+import React, {useState} from "react";
+import {createStyles, makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) =>
+    createStyles({
+        centered: {
+            height: "100vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+        },
+        container: {
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-evenly",
+            alignItems: "center",
+        },
+    })
+);
+
+const Inventors: React.FC = () => {
+    const classes = useStyles();
+
+    const [sponsoredList, setSponsoredList] = useState();
+
+    const [investorList, setInvestorList] = useState();
+
+    const [items, setItems] = useState([
+        {itemName: 'item 1', quantity: 1, isSelected: false},
+        {itemName: 'item 2', quantity: 3, isSelected: true},
+        {itemName: 'item 3', quantity: 2, isSelected: false},
+    ]);
+
+    return (
+        <div>
+            <div className={classes.container}>
+                {/*<div className='add-item-box'>*/}
+                {/*    <input value={inputValue} onChange={(event) => setInputValue(event.target.value)}*/}
+                {/*           className='add-item-input' placeholder='Add an item...'/>*/}
+                {/*</div>*/}
+                <div className='item-list'>
+                    {items.map((item, index) => (
+                        <div className='item-container'>
+                            <div className='item-name' onClick={() => {}}>
+                                {item.isSelected ? (
+                                    <>
+                                        <span className='completed'>{item.itemName}</span>
+                                    </>
+                                ) : (
+                                    <>
+                                        <span>{item.itemName}</span>
+                                    </>
+                                )}
+                            </div>
+                            <div className='quantity'>
+                                <button>
+                                                     onClick={() => {}}/>
+                                </button>
+                                <span> {item.quantity} </span>
+                                <button>
+                                                     onClick={() => {}}/>
+                                </button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Inventors;
--- src/components/View.js
+++ src/components/View.js
@@ -3,6 +3,8 @@ import {AppBar} from "@material-ui/core";
 import {createTheme, makeStyles} from "@material-ui/core/styles";
 import {Paper, Tab, Tabs, ThemeProvider, Typography} from "@mui/material";
 import Bubbles from "./Bubbles";
+import Inventors from "./Inventors";
+import ProjectPage from "./ProjectPage";
 
 const theme = createTheme({
     overrides: {
@@ -61,16 +63,17 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-const View: React.FC<ViewProps> = () => {
+const View: React.FC = () => {
     const classes = useStyles();
 
     const [viewPage, setViewPage] = useState(0);
     const onViewPageChange = (_event, newValue: number) => setViewPage(newValue);
 
     const tabsArray = [
+        <ProjectPage />,
         <Bubbles />,
         <Typography>Muie1</Typography>,
-        <Typography>Muie2</Typography>
+        <Inventors />
     ]
 
     return (
@@ -91,6 +94,8 @@ const View: React.FC<ViewProps> = () => {
                             },
                         }}
                     >
+                        <Tab className={classes.tabStyle} label={<span style={{color: '#FFFCF4'}}>Edit Project</span>}/>
+
                         <Tab className={classes.tabStyle} label={<span style={{color: '#FFFCF4'}}>Ideas</span>}/>
 
                         <Tab className={classes.tabStyle} label={<span style={{color: '#FFFCF4'}}>Builders</span>}/>
